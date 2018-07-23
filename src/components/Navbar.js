ddd2d638eb01a54b9bcbe91280d40cfad62e34b2
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-07-24 04:17:10 +0700

    refactor(Navbar): redo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box, NavLink as N } from 'rebass';
+import { NavLink } from 'rebass';
 import styled from 'styled-components';
-import { colors, navbarHeight } from '../theme';
+import { breakpoints, colors, navbarHeight, space } from '../theme';
 import Anchor from './Anchor';
 import C from './Container';
 
@@ -10,64 +10,78 @@ const borders = {
   bottom: 1,
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.header`
   position: fixed;
   top: 0;
   z-index: 2;
   width: 100%;
+  height: ${navbarHeight}px;
+
   background-color: rgba(255, 255, 255, 0.98);
   border-top: ${borders.top}px solid ${colors.primary};
   border-bottom: ${borders.bottom}px solid ${colors.darken};
-
-  overflow-x: auto;
-  overflow-y: hidden;
-  white-space: nowrap;
-  -ms-overflow-style: -ms-autohiding-scrollbar;
-  -webkit-overflow-scrolling: touch;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
 `;
 
 const Container = C.extend`
   display: flex;
+  flex-wrap: no-wrap;
   height: ${navbarHeight - borders.top - borders.bottom}px;
+
   margin-bottom: 0;
-  padding-left: 0;
-  padding-right: 0;
+  padding-left: ${space[2]}px;
+  padding-right: ${space[2]}px;
 `;
 
-const NavLink = N.extend.attrs({ p: 3, fontSize: 2 })`
-  height: 100%;
+const Nav = styled.nav`
+  display: flex;
+  flex-wrap: no-wrap;
+  margin-left: auto;
+
+  overflow: auto;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  @media (max-width: ${breakpoints[0]}) {
+    mask-image: linear-gradient(
+      to right,
+      transparent,
+      black ${space[3]}px,
+      black 90%,
+      transparent
+    );
+  }
 `;
 
-const MenuItem = NavLink.extend.attrs({
-  px: [2, 3],
+const NavItem = NavLink.extend.attrs({
   color: 'black54',
+  fontSize: 2,
   fontWeight: 'normal',
+  activeClassName: 'navitem-active',
 })`
-  &:hover {
+  &:hover,
+  &.navitem-active {
     color: ${colors.primary};
-    box-shadow: inset 0 -2px ${colors.primary};
-    transition: box-shadow 0.2s ease-in-out;
   }
 `;
 
 export default ({ title, items }) => (
   <Wrapper>
     <Container>
-      <NavLink is={Anchor} to="/">
+      <NavLink is={Anchor} to="/" fontSize={2}>
         {title}
       </NavLink>
 
-      <Box is="nav" ml="auto" px={[2, 0]} style={{ height: '100%' }}>
+      <Nav>
         {items.map(x => (
-          <MenuItem key={x.href} is={Anchor} to={x.href}>
+          <NavItem key={x.href} is={Anchor} to={x.href}>
             {x.label}
-          </MenuItem>
+          </NavItem>
         ))}
-      </Box>
+      </Nav>
     </Container>
   </Wrapper>
 );
